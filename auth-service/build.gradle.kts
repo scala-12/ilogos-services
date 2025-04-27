b083e4fefd53326080a347f09b46750e421f5aa4
Kalashnikov Vladislav <spotless.quash@gmail.com>
2025-04-28 04:21:24 +0800

    Auth-service on Java (Spring Boot+Gradle+JWT)
    Initial commit
    
    [https://start.spring.io/](https://start.spring.io/)
    - Project: **Gradle Kotlin DSL**
    - Language: **Java 23**
    - Dependencies:
      - Spring Web
      - Spring Data JPA
      - PostgreSQL Driver

---
+++ auth-service/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ru.ilogos"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot Starters
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // PostgreSQL
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
