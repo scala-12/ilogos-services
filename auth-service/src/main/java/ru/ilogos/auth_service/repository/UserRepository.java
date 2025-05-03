032f30aaafdfb0add7002e889bff5c77eecece5f
Kalashnikov Vladislav <spotless.quash@gmail.com>
2025-05-04 00:10:28 +0800

    update User: uuid, set methods and builder visibility and functions

--- auth-service/src/main/java/ru/ilogos/auth_service/repository/UserRepository.java
+++ auth-service/src/main/java/ru/ilogos/auth_service/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package ru.ilogos.auth_service.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ru.ilogos.auth_service.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsernameIgnoreCase(String username);
 
     Optional<User> findByEmailIgnoreCase(String email);
