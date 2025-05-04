f0bd96b72ca54c44ba6be98593800baef92b5429
Kalashnikov Vladislav <spotless.quash@gmail.com>
2025-05-05 06:49:09 +0800

    test fix

--- auth-service/src/test/java/ru/ilogos/auth_service/service/JwtServiceTest.java
+++ auth-service/src/test/java/ru/ilogos/auth_service/service/JwtServiceTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +18,21 @@ class JwtServiceTest {
 
     private JwtService jwtService;
 
+    private static User createUser() {
+        try {
+            Method method = User.UserBuilder.class.getDeclaredMethod("id", UUID.class);
+            method.setAccessible(true);
+            var builder = User.builder().username("testuser").role(RoleType.ROLE_ADMIN);
+
+            method.invoke(builder, UUID.randomUUID());
+
+            return builder.build();
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @BeforeEach
     void setUp() {
         jwtService = JwtService.create("verysecretkeyverysecretkeyverysecretkey", 28800000, 604800000);
@@ -22,21 +41,21 @@ class JwtServiceTest {
 
     @Test
     void generateAndValidateAccessToken() {
-        var user = User.builder().username("testuser").role(RoleType.ROLE_ADMIN).build();
+        var user = createUser();
         String token = jwtService.generateToken(user, true);
 
         assertNotNull(token);
 
         var tokenInfo = jwtService.getTokenInfo(token);
         assertEquals(user.getUsername(), tokenInfo.getUsername());
-        assertTrue(tokenInfo.isValid(user));
+        assertTrue(tokenInfo.isValid(user, false));
     }
 
     @Test
     void tokenShouldExpire() throws InterruptedException {
-        var user = User.builder().username("testuser").role(RoleType.ROLE_ADMIN).build();
-        String token = jwtService.generateToken(user, true));
+        var user = createUser();
+        String token = jwtService.generateToken(user, true);
 
-        assertTrue(jwtService.getTokenInfo(token).isValid(user));
+        assertTrue(jwtService.getTokenInfo(token).isValid(user, false));
     }
 }
