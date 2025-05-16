38c7e1c4154430ae3c59dbb771712daaa6ebf6e2
Kalashnikov Vladislav <spotless.quash@gmail.com>
2025-05-17 00:31:52 +0800

    fix(course): jwt filter after oauth2ResourceServer

--- course-service/src/main/kotlin/com/ilogos/course/config/security/SecurityConfig.kt
+++ course-service/src/main/kotlin/com/ilogos/course/config/security/SecurityConfig.kt
@@ -1,27 +1,28 @@
 package com.ilogos.course.config.security
 
+import com.ilogos.course.config.security.filters.JwtAuthenticationFilter
 import com.ilogos.course.jwt.JwtService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig(private val jwtService: JwtService) {
+class SecurityConfig(private val jwtService: JwtService, private val jwtAuthenticationFilter: JwtAuthenticationFilter) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         val builder = http.authorizeHttpRequests({
             it.requestMatchers(
-                "/v3/api-docs/**",
-                "/swagger-ui/**",
-                "/swagger-ui.html"
-            )
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"
+            ).permitAll().anyRequest().authenticated()
         }).oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
+
+        http.addFilterAfter(
+            jwtAuthenticationFilter, BearerTokenAuthenticationFilter::class.java
+        )
 
         return builder.build()
     }
