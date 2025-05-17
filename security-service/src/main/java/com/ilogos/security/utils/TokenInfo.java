ed33bb69672da0d22c3bd04c1c6a378b626d459f
Kalashnikov Vladislav <spotless.quash@gmail.com>
2025-05-17 08:22:10 +0800

    feat(security): TokenInfo for Jwt

--- security-service/src/main/java/com/ilogos/security/utils/TokenInfo.java
+++ security-service/src/main/java/com/ilogos/security/utils/TokenInfo.java
@@ -4,17 +4,27 @@ import java.security.PublicKey;
 import java.util.Date;
 import java.util.UUID;
 
+import org.springframework.security.oauth2.jwt.Jwt;
+
 import com.ilogos.security.user.model.IUser;
 import com.ilogos.security.user.model.IUserBase;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.Getter;
 
 @Getter
 public class TokenInfo implements IUserBase {
-    private final Claims claims;
+    public final static String USERNAME_CLAIM = "username";
+    public final static String EMAIL_CLAIM = "email";
+    public final static String TYPE_CLAIM = "type";
+
     private final String token;
+    private final Date expiration;
+    private final String username;
+    private final UUID id;
+    private final String email;
+    private final Date issuedAt;
+    private final String type;
 
     private enum Type {
         ACCESS,
@@ -24,28 +34,48 @@ public class TokenInfo implements IUserBase {
 
     public TokenInfo(String token, PublicKey publicKey) {
         this.token = token;
-        this.claims = Jwts.parserBuilder()
+        var claims = Jwts.parserBuilder()
                 .setSigningKey(publicKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+        expiration = claims.getExpiration();
+        username = claims.get(USERNAME_CLAIM, String.class);
+        id = UUID.fromString(claims.getSubject());
+        email = claims.get(EMAIL_CLAIM, String.class);
+        issuedAt = claims.getIssuedAt();
+        type = claims.get(TYPE_CLAIM, String.class);
+    }
+
+    public TokenInfo(Jwt jwt) {
+        token = jwt.getTokenValue();
+
+        expiration = Date.from(jwt.getExpiresAt());
+        username = jwt.getClaimAsString(USERNAME_CLAIM);
+        id = UUID.fromString(jwt.getSubject());
+        email = jwt.getClaimAsString(EMAIL_CLAIM);
+        issuedAt = Date.from(jwt.getIssuedAt());
+        type = jwt.getClaimAsString(TYPE_CLAIM);
     }
 
     public boolean isExpired() {
-        return claims.getExpiration().before(new Date());
+        return expiration.before(new Date());
     }
 
     public boolean isValid(IUser user, boolean checkIat) {
-        return user != null && user.getUsername().equals(getUsername()) && !isExpired()
-                && (!checkIat || !getIssuedAt().toInstant().isBefore(user.getLastTokenIssuedAt()));
+        return user != null && user.getUsername().equals(username) && !isExpired()
+                && (!checkIat || !issuedAt.toInstant().isBefore(user.getLastTokenIssuedAt()));
+    }
+
+    public static boolean isAccessType(String type) {
+        return Type.ACCESS.equals(getType(type));
     }
 
     public boolean isValid(IUser user) {
         return isValid(user, true);
     }
 
-    public Type getType() {
-        String type = claims.get("type", String.class);
+    private static Type getType(String type) {
         if (type == null) {
             return Type.UNDEFINED;
         }
@@ -57,30 +87,15 @@ public class TokenInfo implements IUserBase {
         };
     }
 
+    public Type getType() {
+        return getType(type);
+    }
+
     public boolean isAccess() {
         return Type.ACCESS.equals(getType());
     }
 
     public boolean isRefresh() {
         return Type.REFRESH.equals(getType());
-    }
-
-    public Date getIssuedAt() {
-        return claims.getIssuedAt();
-    }
-
-    @Override
-    public String getEmail() {
-        return claims.get("email", String.class);
-    }
-
-    @Override
-    public UUID getId() {
-        return UUID.fromString(claims.getSubject());
-    }
-
-    @Override
-    public String getUsername() {
-        return claims.get("username", String.class);
     }
 }
