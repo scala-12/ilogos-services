92403fda34e0bc9ca64c32bb2fdbdff0ad730677
Kalashnikov Vladislav <spotless.quash@gmail.com>
2025-05-20 23:24:29 +0800

    chore(auth): postrges plugin removed

--- auth-service/plugins/postgres.ts
+++
@@ -1,8 +0,0 @@
-import PostgresPlugin from '@fastify/postgres';
-import fp from 'fastify-plugin';
-
-export default fp(async function (fastify) {
-  fastify.register(PostgresPlugin, {
-    connectionString: process.env.DATABASE_URL,
-  });
-});
